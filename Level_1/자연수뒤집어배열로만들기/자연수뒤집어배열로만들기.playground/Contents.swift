61d8db772eec66e32afc2f8ab2187e7cd9d08dfa
Dylan-Yoon <d.dylany21@gmail.com>
2022-07-15 19:45:46 +0900

    자연수뒤집어배열로만들기 : readme작성 및 solution2 추가

--- Level_1/자연수뒤집어배열로만들기/자연수뒤집어배열로만들기.playground/Contents.swift
+++ Level_1/자연수뒤집어배열로만들기/자연수뒤집어배열로만들기.playground/Contents.swift
@@ -1,12 +1,26 @@
-func solution(_ n: Int64) -> [Int] {
+//func solution(_ n: Int64) -> [Int] {
+//    var result = [Int]()
+//    var figure = 1
+//    for _ in 0 ... String(n).count - 1 {
+//        figure = figure * 10
+//        result.append((Int(n) % figure)/(figure/10))
+//    }
+//    return result
+//}
+//solution(12345)
+//solution(19412320)
+
+func solution2(_ n: Int64) -> [Int] {
+    let a = String(n)
+    let b = Array(a)
+    let c = b.reversed() //역순 배열로 준비
     var result = [Int]()
-    var figure = 1
-    for _ in 0 ... String(n).count - 1 {
-        figure = figure * 10
-        result.append((Int(n) % figure)/(figure/10))
+    for i in c {
+        result.append(Int(String(i))!)
     }
     return result
 }
+solution2(12345)
+solution2(19412320)
 
-solution(12345)
-solution(19412320)
+
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Swift_Programmers
+
+# 자연수 뒤집어 배열로 만들기
+2가지 경우의 수로 만들었음 
+solution 의 경우 숫자를 각 자리수에 따라 새로운 결과 result 배열에 추가하는 방식
+
+solution2 의 경우 타입을 이용해 reversed() 하여 문제해결
