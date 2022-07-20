07ee6dea9bc60d84b1c1283ffa568551fb6530b7
dylan-yoon <d.dylany21@gmail.com>
2022-07-20 17:05:35 +0900

    시저암호 : Pass

--- Level_1/시저암호/시저암호.playground/Contents.swift
+++ Level_1/시저암호/시저암호.playground/Contents.swift
@@ -1,19 +1,32 @@
 func solution(_ s:String, _ n:Int) -> String {
+    var solve = ""
     let lowerAlpa = Array("abcdefghijklmnopqrstuvwxyz")
     let upperAlpa = Array("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    
-    var solve = ""
+
     
     for element in s {
-        if element.isLowercase {
-            lowerAlpa.
+        if element == " " {
+            solve += " "
+        } else if element.isLowercase {
+            var a = lowerAlpa.firstIndex(of: element)! + n
+            if a > 25 {
+                a = a - 26
+            }
+            solve += String(lowerAlpa[a])
         } else {
-            
+            var a = upperAlpa.firstIndex(of: element)! + n
+            if a > 25 {
+                a = a - 26
+            }
+            solve += String(upperAlpa[a])
         }
     }
-
-    
-    return ""
+    return solve
 }
 
-solution("aiekAIEK", 0)
+solution("aiekAIEK", 1)
+
+
+
+
+// 입력이 z 이면 인덱스 25 이고 n= 1 이면 26이상일 때 26을 뺀다
