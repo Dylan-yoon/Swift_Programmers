ef59c0b5d926e6ecec882313f944f5f5d65bef41
Dylan-Yoon <d.dylany21@gmail.com>
2022-07-20 16:21:19 +0900

    시저암호 임시 커밋

---
+++ Level_1/시저암호/시저암호.playground/Contents.swift
@@ -0,0 +1,19 @@
+func solution(_ s:String, _ n:Int) -> String {
+    let lowerAlpa = Array("abcdefghijklmnopqrstuvwxyz")
+    let upperAlpa = Array("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    
+    var solve = ""
+    
+    for element in s {
+        if element.isLowercase {
+            lowerAlpa.
+        } else {
+            
+        }
+    }
+
+    
+    return ""
+}
+
+solution("aiekAIEK", 0)
